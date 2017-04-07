0ba2c04b8d24988403c850043d7d584ecdb42346
Tyler Burton <btburton42@gmail.com>
2017-04-07 15:13:37 -0700

    added new API and WEB package. working UI
    dockerized Twittervotes
     -- no seed for DB (should be resolved by dockerizing Web)
    
    dockerized and working
    
    cleanup

--- twittervotes/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-export SP_TWITTER_KEY=P12Xiq05uLH3UUftN7qvZnwfZ
-export SP_TWITTER_ACCESSTOKEN=15829179-apajiZKemWaYyx93PbvxuylXfcxjwzOJtFU3GfHbh
-export SP_TWITTER_SECRET=Qhs6YuDUerBkwtH8etVtgfKocyVuONh7bravwKTYU0GJfVkbac
-export SP_TWITTER_ACCESSSECRET=oYvXF6JP2Lyx0GoOuTdHJZgp4n8JSAOjgkoTspUUExoiK
