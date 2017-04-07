109ea092a27c486b05585c2b06c2d6a472ee3794
Tyler Burton <btburton42@gmail.com>
2017-04-07 15:13:37 -0700

    added new API and WEB package. working UI

---
+++ api/respond.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func respond(w http.ResponseWriter, r *http.Request, status int, data interface{},) {
+	w.WriteHeader(status)
+	if data != nil {
+		encodeBody(w, r, data)
+	}
+}
+
+func respondErr(w http.ResponseWriter, r *http.Request, status int, args ...interface{},) {
+	respond(w, r, status, map[string]interface{}{
+		"error": map[string]interface{}{
+			"message": fmt.Sprint(args...),
+		},
+	})
+}
+
+func respondHTTPErr(w http.ResponseWriter, r *http.Request, status int,) {
+	respondErr(w, r,status, http.StatusText(status))
+}
